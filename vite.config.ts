c2f4c70efe77e68c02371a0795f7c7980176849f
Shashwat Tripathi <tshashwat409@gmail.com>
2025-07-09 02:00:01 +0530

    Fix base path for Netlify and add redirects

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/shashwatportfolio/", // 👈 ADD THIS LINE
+  base: "/", // 👈 ADD THIS LINE
   server: {
     host: "::",
     port: 8080,
